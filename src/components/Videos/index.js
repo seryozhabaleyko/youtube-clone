229ea250569075e33d981d5927d5fe0de4a47d22
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-01 13:42:16 +0300

    Create index.js

---
+++ src/components/Videos/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import VideoCard from '../VideoCard';
+
+import './videos.scss';
+
+function Videos() {
+    return (
+        <section className="videos">
+            <h2>Videos</h2>
+            <div className="videos__grid">
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <VideoCard
+                    title="Клава Кока & NILETTO - КРАШ (Acoustic version, 2020)"
+                    image="https://i.ytimg.com/vi/Kz917EsnUtQ/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCGTnkskx6D2Vcpp1QluY76F7yOQ"
+                    views="420 тыс. просмотров"
+                    timestamp="20 часов назад"
+                    channel="Клава Кока"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJy2nE-9kHbB5O8Yd0LtUUz5Yjvjm0SK6SRkbMviYQ=s88-c-k-c0x00ffffff-no-rj"
+                />
+            </div>
+        </section>
+    );
+}
+
+export default Videos;
