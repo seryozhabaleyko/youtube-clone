38e644a8aeb6e888f6a04da770304d0fce4a2b53
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-01 13:42:48 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyC-kh1qrJ-gaug1s4H-DNO5vd5Dc7oYoCs',
+    authDomain: 'clone-81e5b.firebaseapp.com',
+    databaseURL: 'https://clone-81e5b.firebaseio.com',
+    projectId: 'clone-81e5b',
+    storageBucket: 'clone-81e5b.appspot.com',
+    messagingSenderId: '1010970341930',
+    appId: '1:1010970341930:web:8d2fbe4f6fcb3651370953',
+};
